6dd6236a1863ba94ea41bec8f30ffa21e24a7365
cpramshad <cpramshad@gmail.com>
2020-09-02 16:34:44 +0400

    [feature] - adding local storage for likes

--- src/js/index.js
+++ src/js/index.js
@@ -96,10 +96,13 @@ const controlRecipe = async () => {
             recipeView.renderRecipe(state.recipe, state.likes.isLiked(id));
            
         } catch (error) {
+            console.log(error);
             alert('Error while processing recipe!');
         }
     }
 }
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
 /**
  * List Controller
@@ -137,9 +140,6 @@ elements.shopping.addEventListener('click', e => {
 /**
  * Like Controller
  */
-// Testing
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
 
 const controlLike = () => {
     if(!state.likes) state.likes = new Likes();
@@ -177,10 +177,26 @@ const controlLike = () => {
 
 
 
+
+
 // window.addEventListener('hashchange', controlRecipe);
 // window.addEventListener('load', controlRecipe);
 
-['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+
+// Restore liked recipe when page loads
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 elements.recipe.addEventListener('click', e => {
     // Decrease button is clicked
